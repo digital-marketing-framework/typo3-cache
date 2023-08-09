f09efa7c5bd79dbd74ed55d60e28ba756690a0d5
Stephan Ude <ude@mediatis.de>
2023-08-09 19:49:43 +0200

    [TASK] configuration document version handling

--- Classes/Registry/EventListener/CoreRegistryUpdateEventListener.php
+++ Classes/Registry/EventListener/CoreRegistryUpdateEventListener.php
@@ -3,18 +3,23 @@
 namespace DigitalMarketingFramework\Typo3\Cache\Registry\EventListener;
 
 use DigitalMarketingFramework\Core\Cache\DataCache;
+use DigitalMarketingFramework\Core\Registry\RegistryUpdateType;
 use DigitalMarketingFramework\Typo3\Cache\Cache\Typo3Cache;
-use DigitalMarketingFramework\Typo3\Core\Registry\Event\CoreRegistryServiceUpdateEvent;
+use DigitalMarketingFramework\Typo3\Core\Registry\Event\CoreRegistryUpdateEvent;
 
-class CoreRegistryServiceUpdateEventListener
+class CoreRegistryUpdateEventListener
 {
     public function __construct(
         protected Typo3Cache $cache,
     ) {
     }
 
-    public function __invoke(CoreRegistryServiceUpdateEvent $event): void
+    public function __invoke(CoreRegistryUpdateEvent $event): void
     {
+        if ($event->getUpdateType() !== RegistryUpdateType::SERVICE) {
+            return;
+        }
+
         $registry = $event->getRegistry();
         $event->getRegistry()->setCache(
             $registry->createObject(DataCache::class, [$this->cache])
